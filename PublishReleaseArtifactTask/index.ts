e316775c4f8d5a60208c3b3c36a5521fcfc09e7c
Benj Huser <benj.huser@gmail.com>
2020-02-01 21:24:07 +0100

    Add publish task

--- PublishReleaseArtifactTask/index.ts
+++ PublishReleaseArtifactTask/index.ts
@@ -1,17 +1,57 @@
 import tl = require('azure-pipelines-task-lib/task');
+import fs = require('fs');
+import path = require('path');
+var zipper = require('zip-local');
 
 async function run() {
    try {
-      const inputString: string | undefined = tl.getInput('artifactName', true);
-      if (inputString == 'bad') {
-         tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
-         return;
+      var artifactPath: string = getVariable('artifactPath', true);
+      var outputVariable: string = getVariable('outputVariable', false);
+
+      if (!fs.existsSync(artifactPath)) {
+         tl.setResult(tl.TaskResult.Failed, `No file or directory found at ${artifactPath}`);
       }
-      console.log('Hello', inputString);
+
+      if (fs.lstatSync(artifactPath).isDirectory()) {
+         console.log(`${artifactPath} is a directory - will zip.`)
+         artifactPath = zip_file(artifactPath);
+      }
+      else {
+         console.log(`${artifactPath} is a file.`)
+      }
+
+      console.log(`##vso[task.uploadfile]${artifactPath}`)
+      if (outputVariable != "") {
+         var fileName: string = path.basename(artifactPath);
+
+         console.log(`##vso[task.setvariable variable=${outputVariable};]${fileName}`);
+      }
+
    }
    catch (err) {
       tl.setResult(tl.TaskResult.Failed, err.message);
    }
+}
+
+function zip_file(artifactPath: string): string {
+   var artifact_zip: string = `${artifactPath}.zip`;
+   zipper.sync.zip(artifactPath).compress().save(artifact_zip);
+   return artifact_zip;
+}
+
+function getVariable(variableName: string, isRequired: boolean): string {
+   const variable: string | undefined = tl.getInput(variableName, isRequired);
+   if (variable === null || variable === undefined) {
+      if (isRequired) {
+         tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
+         throw new Error(`Variable ${variableName} not found`);
+      }
+      else {
+         return "";
+      }
+   }
+
+   return variable;
 }
 
 run();
